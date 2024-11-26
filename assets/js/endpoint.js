e90ba3f69ba73f46305c23bf4eb8f2c0606675fe
Ibrohim Mubarok <befousmain@gmail.com>
2024-11-26 19:35:44 +0700

    Update endpoint

--- assets/js/endpoint.js
+++ assets/js/endpoint.js
@@ -1,6 +1,6 @@
-export let APILogin = "https://freight-trackr-backend-48667702974.asia-southeast2.run.app/login";
-export let APIRegister = "https://freight-trackr-backend-48667702974.asia-southeast2.run.app/register";
-export let APIUsers = "https://freight-trackr-backend-48667702974.asia-southeast2.run.app/users";
-export let APITransaksi = "https://freight-trackr-backend-48667702974.asia-southeast2.run.app/transaksi";
-export let APIKantor = "https://freight-trackr-backend-48667702974.asia-southeast2.run.app/kantor";
-export let APIPelanggan = "https://freight-trackr-backend-48667702974.asia-southeast2.run.app/pelanggan";
+export let APILogin = "https://asia-southeast2-befous.cloudfunctions.net/FreightTrackr/login";
+export let APIRegister = "https://asia-southeast2-befous.cloudfunctions.net/FreightTrackr/register";
+export let APIUsers = "https://asia-southeast2-befous.cloudfunctions.net/FreightTrackr/users";
+export let APITransaksi = "https://asia-southeast2-befous.cloudfunctions.net/FreightTrackr/transaksi";
+export let APIKantor = "https://asia-southeast2-befous.cloudfunctions.net/FreightTrackr/kantor";
+export let APIPelanggan = "https://asia-southeast2-befous.cloudfunctions.net/FreightTrackr/pelanggan";
