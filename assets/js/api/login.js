ff625851196d3cd6192284e1f8e85cac28b30930
Ibrohim Mubarok <befousmain@gmail.com>
2024-12-22 21:12:17 +0700

    Rubah dari alert menjadi sweetalert2

--- assets/js/api/login.js
+++ assets/js/api/login.js
@@ -3,6 +3,7 @@ import { setCookieWithExpireHour } from "https://cdn.jsdelivr.net/gh/jscroot/lib
 import { redirect } from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.1.6/url.js";
 import { APILogin } from "../endpoint.js"
 import { postBiasa } from "../api.js";
+import sweetalert2 from 'https://cdn.jsdelivr.net/npm/sweetalert2@11.15.3/+esm'
 
 export default function Login(){
     let datajson = {
@@ -14,10 +15,23 @@ export default function Login(){
 
 function responseFunction(result) {
     if (result.status == 200) {
-        alert(result.data.message);
-        setCookieWithExpireHour("Authorization", result.data.token, 2);
-        redirect("../"); 
+        sweetalert2.fire({
+            title: 'Success!',
+            text: result.data.message,
+            icon: 'success',
+            confirmButtonText: 'Ok'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                setCookieWithExpireHour("Authorization", result.data.token, 2);
+                redirect("../"); 
+            }
+        });
     } else {
-        alert(result.data.message);
+        sweetalert2.fire({
+            title: 'Info!',
+            text: result.data.message,
+            icon: 'info',
+            confirmButtonText: 'Ok'
+        })
     }
 }
