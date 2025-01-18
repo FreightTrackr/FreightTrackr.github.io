54a9272837a7632363a7f9e93ec34bf025dc5e43
Ibrohim Mubarok <befousmain@gmail.com>
2025-01-18 21:52:21 +0700

    Buat agar button disable ketika ditekan

--- assets/js/api/login.js
+++ assets/js/api/login.js
@@ -6,6 +6,10 @@ import { postBiasa } from "../api.js";
 import sweetalert2 from 'https://cdn.jsdelivr.net/npm/sweetalert2@11.15.3/+esm'
 
 export default function Login(){
+    const button = document.getElementById("login-button");
+    button.disabled = true;
+    button.classList.add("opacity-50", "cursor-not-allowed");
+    button.classList.remove("active:bg-purple-600", "hover:bg-purple-700", "focus:shadow-outline-purple");
     let datajson = {
         "username": getValue("username"),
         "password": getValue("password")
@@ -34,4 +38,8 @@ function responseFunction(result) {
             confirmButtonText: 'Ok'
         })
     }
+    const button = document.getElementById("login-button");
+    button.disabled = false;
+    button.classList.remove("opacity-50", "cursor-not-allowed");
+    button.classList.add("active:bg-purple-600", "hover:bg-purple-700", "focus:shadow-outline-purple");
 }
