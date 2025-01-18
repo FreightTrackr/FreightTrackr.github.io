32d57293b6d02bcf95cf89b064027bd0ac8e58b5
Ibrohim Mubarok <befousmain@gmail.com>
2025-01-18 21:52:32 +0700

    Buat agar button disable ketika ditekan

--- assets/js/api/register.js
+++ assets/js/api/register.js
@@ -6,6 +6,10 @@ import { postJSON } from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.1.6/api.js";
 import sweetalert2 from 'https://cdn.jsdelivr.net/npm/sweetalert2@11.15.3/+esm'
 
 export default function Register(){
+    const button = document.getElementById("register-button");
+    button.disabled = true;
+    button.classList.add("opacity-50", "cursor-not-allowed");
+    button.classList.remove("active:bg-purple-600", "hover:bg-purple-700", "focus:shadow-outline-purple");
     const tokenkey = "Authorization"
     let tokenvalue = getCookie(tokenkey)
     let datajson = {
@@ -39,4 +43,8 @@ function responseFunction(result) {
             confirmButtonText: 'Ok'
         })
     }
+    const button = document.getElementById("register-button");
+    button.disabled = false;
+    button.classList.remove("opacity-50", "cursor-not-allowed");
+    button.classList.add("active:bg-purple-600", "hover:bg-purple-700", "focus:shadow-outline-purple");
 }
