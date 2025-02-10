771b02cbf0d28bf1efd160be044e171d9ff04116
Ibrohim Mubarok <befousmain@gmail.com>
2025-02-10 08:33:23 +0700

    Buat function untuk get csv

--- assets/js/api.js
+++ assets/js/api.js
@@ -94,3 +94,34 @@ export function deleteBiasa(target_url,datajson,responseFunction){
         })
         .catch(error => console.log('error', error));
 }
+
+export function getCsv(target_url, tokenkey, tokenvalue, responseFunction) {
+    let myHeaders = new Headers();
+    myHeaders.append(tokenkey, tokenvalue);
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Accept", "text/csv");
+
+    let requestOptions = {
+        method: 'GET',
+        redirect: 'follow',
+        headers: myHeaders
+    };
+
+    fetch(target_url, requestOptions)
+        .then(response => {
+            const status = response.status;
+            return response.text().then(result => {
+                if (response.ok) {
+                    responseFunction({ status, data: result });
+                } else {
+                    try {
+                        const parsedError = JSON.parse(result);
+                        responseFunction({ status, data: parsedError.message });
+                    } catch {
+                        responseFunction({ status, data: result });
+                    }
+                }
+            });
+        })
+        .catch(error => console.error('Error fetching data:', error));
+}
